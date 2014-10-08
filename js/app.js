c48931e10907a1ecb0b59ca9433e3547fb85ef70
Kristjan Jansen <kristjan.jansen@gmail.com>
2014-10-08 23:08:28 +0300

    latest

--- js/app.js
+++ js/app.js
@@ -2,48 +2,63 @@ $(function() {
 
   $.get('data/states.yml', function(data) {
     
-    var states = yaml.load(data)
+  var states = yaml.load(data)
   
   for (var key in states) {
     if (states[key].frontpage) {
-      currentState = key
+      var currentState = key
+    }
+    if (states[key].button_sound) {
+      var buttonSound = states[key].button_sound
     }
   }
-
+  
+  var currentLanguage = defaultLanguage = 'et'
+  
   renderState(currentState)
 
 
 $('.button').on('click', function() {
   
-  var id = $(this).parent().attr('id')
-  if (sound = states[currentState][id].button_sound || null) {
+  $('#firstSound').attr('src','audio/' + buttonSound).trigger('play')
 
-    playSound(sound)
+  var id = $(this).parent().attr('id')
+
+  if (language = states[currentState][id].language) {
+    currentLanguage = language
   }
+
+  if (states[currentState][id].go && (introSound = states[states[currentState][id].go].intro_sound)) { 
+    $('#secondSound').attr('src','audio/' + introSound).trigger('play')
+  }
+    
   currentState = states[states[currentState][id].go] ? states[currentState][id].go : currentState
   renderState(currentState)
  
   if (states[currentState].timer) {
     setTimeout(function() {
       currentState = states[states[currentState].timer.go] ? states[currentState].timer.go : currentState
+      $('#secondSound').trigger('pause')
       renderState(currentState)
+      
     }, states[currentState].timer.time)
   }
   
-    
+  console.log('Current language: ', currentLanguage)
+
 })
 
-function playSound(sound) {
-  $('audio').attr('src','audio/' + sound).trigger('play')
-}
-
 function renderState(currentState) {
-  $('.title').html(states[currentState].title)
+  var title_key = (currentLanguage !== defaultLanguage ? 'title_' + currentLanguage : 'title') 
+  var label_key = (currentLanguage !== defaultLanguage ? 'label_' + currentLanguage : 'label') 
+   
+  $('.title').html(states[currentState][title_key] ? states[currentState][title_key] : states[currentState]['title'])
+  
   for (var key in states[currentState]) {
     if (key.length < 4 && key.substr(0,1).toLowerCase() == 'b') {
-    if (states[currentState][key].label) {    
+    if (states[currentState][key][label_key] || states[currentState][key]['label']) {
       $('#' + key + ' .label').removeClass('hidden')
-      $('#' + key + ' .label').text(states[currentState][key].label)
+      $('#' + key + ' .label').text(states[currentState][key][label_key] ? states[currentState][key][label_key] : states[currentState][key]['label'])
     } else {
       $('#' + key + ' .label').addClass('hidden')
     }
