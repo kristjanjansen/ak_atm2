a5e3820579af2e5707e617d3c686a74698f1cc7d
disain <disain@eitea.ametikool.local>
2014-10-28 11:46:14 +0200

    latest

--- update2.js
+++ update2.js
@@ -5,7 +5,7 @@ var path = '/Volumes/dk/Projektid/MK-13/veebitehnoloogiad/'
 
 setInterval(function() {
 
-child = exec('cp ' + path + 'states-* data/. && cat $(ls -t data/states-*) > data/states_all.yml',
+child = exec('cp ' + path + 'states-* data/. && cat $(ls -t -1 data/states-* | sort) > data/states_all.yml',
   function (error, stdout, stderr) {
   	console.log('Updating state files')
     console.log(stdout);
